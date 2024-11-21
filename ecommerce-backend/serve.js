fe5e19737d14c1f26be742c5b9e9d3901f8205d7
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2024-11-21 17:49:48 -0300

    Update serve.js

--- ecommerce-backend/serve.js
+++ ecommerce-backend/serve.js
@@ -5,8 +5,10 @@ const authRoutes = require('./routes/auth');
 const app = express()
 const PORT = 3000;
 
+//Middleware para ler as requisições HTPP que vierem do front
 app.use(bodyParser.json())
 
+//Rotas(Isso é o endereço que você vai chamar no front, isso é o que sera executado)
 app.use('./api/auth', authRoutes);
 
 app.listem(PORT, ()=>{
